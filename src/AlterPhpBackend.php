a423ab59f6659402258bd5224d0d73dc92bd0784
Punk_UnDeaD <punkundead@gmail.com>
2016-05-14 19:03:54 +0300

    beauty output, simplify and refactoring

--- src/AlterPhpBackend.php
+++ src/AlterPhpBackend.php
@@ -20,55 +20,26 @@ class AlterPhpBackend extends PhpBackend {
 
   function drupal_var_export($var, $prefix = '') {
     if (is_array($var)) {
-      if (empty($var)) {
-        $output = '[]';
+      $output = [];
+      $export_keys = array_values($var) != $var;
+      foreach ($var as $key => $value) {
+        $output[] = ($export_keys ? var_export($key, TRUE) . ' => ' : '') . $this->drupal_var_export($value, $prefix . '  ');
+      }
+      if (strlen($output_imp = implode(', ', $output)) > 120) {
+        return "[\n$prefix  " . implode(",\n  $prefix", $output) . "\n$prefix]";
       }
       else {
-        $output = "[\n";
-        // Don't export keys if the array is non associative.
-        $export_keys = array_values($var) != $var;
-        foreach ($var as $key => $value) {
-          $output .= '  ' . ($export_keys ? $this->drupal_var_export($key) . ' => ' : '') . $this->drupal_var_export($value, '  ', FALSE) . ",\n";
-        }
-        $output .= ']';
+        return "[$output_imp]";
       }
     }
-    elseif (is_bool($var)) {
-      $output = $var ? 'TRUE' : 'FALSE';
-    }
-    elseif (is_string($var)) {
-      $line_safe_var = str_replace("\n", '\n', $var);
-      if (strpos($var, "\n") !== FALSE || strpos($var, "'") !== FALSE) {
-        // If the string contains a line break or a single quote, use the
-        // double quote export mode. Encode backslash and double quotes and
-        // transform some common control characters.
-        $var = str_replace(array('\\', '"', "\n", "\r", "\t"), array('\\\\', '\"', '\n', '\r', '\t'), $var);
-        $output = '"' . $var . '"';
-      }
-      else {
-        $output = "'" . $var . "'";
-      }
-    }
-    elseif (is_object($var)) {
-      // var_export() will export stdClass objects using an undefined
-      // magic method __set_state() leaving the export broken. This
-      // workaround avoids this by casting the object as an array for
-      // export and casting it back to an object when evaluated.
+    if (is_object($var)) {
       if (get_class($var) === 'stdClass') {
-        $output = '(object) ' . $this->drupal_var_export((array) $var, $prefix);
+        return '(object) ' . $this->drupal_var_export((array) $var, $prefix);
       }
       else {
-        $output = 'unserialize(' . $this->drupal_var_export(serialize($var)) . ')';
+        return 'unserialize(' . $this->drupal_var_export(serialize($var)) . ')';
       }
     }
-    else {
-      $output = var_export($var, TRUE);
-    }
-
-    if ($prefix) {
-      $output = str_replace("\n", "\n$prefix", $output);
-    }
-
-    return $output;
+    return var_export($var, TRUE);
   }
 }
